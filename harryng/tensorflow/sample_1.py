05225d4cfb441a61c14428ca51c93e520d5dd862
harryng <little.ruan@gmail.com>
2021-06-27 00:20:12 +0700

    learn tf

--- harryng/tensorflow/sample_1.py
+++ harryng/tensorflow/sample_1.py
@@ -1,11 +1,15 @@
 import time
 import pathlib
+
+import numpy as np
 import tensorflow as tf
+import numpy as numpy
 import matplotlib.pyplot as plt
 
 this_path = str(pathlib.Path(__file__).parent.parent.parent.resolve()) + "/data"
 pathlib.Path(this_path).mkdir(parents=True, exist_ok=True)
 data_file = pathlib.Path(this_path + "/mnist.npz")
+data_model= pathlib.Path(this_path + "/model.mol")
 
 tf.random.set_seed(time.time())
 mnist = tf.keras.datasets.mnist
@@ -22,6 +26,17 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10)
 ])
+
+# model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.1), loss=tf.keras.losses.mse)
+# model.fit(x_train, y_train, epochs=1)
+# tf.keras.models.save_model(model=model, filepath=data_model, overwrite=True)
+
+model = tf.keras.models.load_model(filepath=data_model, compile=False)
+prediction = np.argmax(model.predict(np.reshape(x_test[5], (1, 28, 28))))
+print(f"prediction: {prediction}")
+
+plt.imshow(x_test[5])
+plt.show()
 
 predictions = model(x_train[:1]).numpy()
 print(f"predictions: {predictions}")
